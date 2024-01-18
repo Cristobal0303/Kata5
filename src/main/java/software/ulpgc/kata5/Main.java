1a0e81a7909bbe5c85b098a1f0502154dbf13518
Cristóbal Sarmiento Rodríguez <cristobal.sarmiento101@alu.ulpgc.es>
2024-01-18 20:20:42 +0000

    Class Main created for execution.

---
+++ src/main/java/software/ulpgc/kata5/Main.java
@@ -0,0 +1,58 @@
+package software.ulpgc.kata5;
+
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        commands.put("factorial", new FactorialCommand());
+        commands.put("fibonacci", new FibonacciCommand());
+
+        Spark.port(8080);
+
+        Spark.get("/factorial/:number", (req, res) -> new CommandExecutor(req, res).execute("factorial"));
+        Spark.get("/fibonacci/:number", (req, res) -> new CommandExecutor(req, res).execute("fibonacci"));
+    }
+
+    static Map<String, Command> commands = new HashMap<>();
+    public static class CommandExecutor {
+        private final Request request;
+        private final Response response;
+
+        public CommandExecutor(Request request, Response response) {
+
+            this.request = request;
+            this.response = response;
+
+        }
+
+        public String execute(String name) {
+
+            Command command = commands.get(name);
+            Command.Output output = command.execute(input());
+            response.status(output.responseCode());
+
+            return output.result();
+
+        }
+
+        private Command.Input input() {
+
+            return parameter -> oneOf(request.params(parameter), request.queryParams(parameter));
+
+        }
+
+        private String oneOf(String a, String b) {
+
+            return a != null ? a : b;
+
+        }
+    }
+
+}
